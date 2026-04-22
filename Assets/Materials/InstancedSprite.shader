3beb7ac7f5a6b98f714072df3ac2880615c2b7e7
Nikolay0525 <annaksota361@gmail.com>
2026-04-23 01:51:34 +0300

    Added trees

---
+++ Assets/Materials/InstancedSprite.shader
@@ -0,0 +1,60 @@
+Shader "Custom/InstancedSprite" {
+    Properties {
+        _MainTex ("Texture", 2D) = "white" {}
+        _Color ("Tint", Color) = (1,1,1,1)
+    }
+    SubShader {
+        // Налаштування для прозорості
+        Tags {"Queue"="Transparent" "IgnoreProjector"="True" "RenderType"="Transparent"}
+        LOD 100
+        ZWrite Off
+        Blend SrcAlpha OneMinusSrcAlpha
+        
+        // ВАЖЛИВО: Вимикаємо відсікання "спини". Тепер Quad видно з обох боків!
+        Cull Off 
+
+        Pass {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            
+            // ВАЖЛИВО: Цей рядок вмикає підтримку GPU Instancing!
+            #pragma multi_compile_instancing 
+            
+            #include "UnityCG.cginc"
+
+            struct appdata {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+                UNITY_VERTEX_INPUT_INSTANCE_ID
+            };
+
+            struct v2f {
+                float2 uv : TEXCOORD0;
+                float4 vertex : SV_POSITION;
+                UNITY_VERTEX_INPUT_INSTANCE_ID
+            };
+
+            sampler2D _MainTex;
+            float4 _MainTex_ST;
+            fixed4 _Color;
+
+            v2f vert (appdata v) {
+                v2f o;
+                UNITY_SETUP_INSTANCE_ID(v);
+                UNITY_TRANSFER_INSTANCE_ID(v, o);
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+                return o;
+            }
+
+            fixed4 frag (v2f i) : SV_Target {
+                UNITY_SETUP_INSTANCE_ID(i);
+                // Читаємо текстуру і множимо на колір
+                fixed4 col = tex2D(_MainTex, i.uv) * _Color;
+                return col;
+            }
+            ENDCG
+        }
+    }
+}
--- Assets/Scripts/TerrainChunk.cs
+++ Assets/Scripts/TerrainChunk.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 
 [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer), typeof(EdgeCollider2D))]
@@ -19,12 +20,26 @@ public class TerrainChunk : MonoBehaviour
     private Mesh mesh;
     private EdgeCollider2D edgeCollider;
 
+    [Header("GPU Instanced Trees")]
+    public Mesh treeMesh;           // Сюди закинь стандартний Quad (з вікна проектів)
+    public Material[] treeMaterials;   // Матеріал дерева З УВІМКНЕНИМ GPU Instancing
+    [Range(0f, 1f)] public float treeDensity = 0.1f;
+    public float minTreeDistance = 2f;
+    public float maxSlopeAngle = 25f;
+
+    private Vector3[][] treeLocalPositionsPerType;
+    private Vector3[][] treeScalesPerType;
+    private Matrix4x4[][] treeMatricesPerType;
+
     private class ChunkData
     {
         public Vector3[] vertices;
         public int[] triangles;
         public Vector2[] uvs;
         public Vector2[] colliderPoints;
+
+        public Vector3[][] localPositionsPerType;
+        public Vector3[][] scalesPerType;
     }
 
     private void Awake()
@@ -32,6 +47,36 @@ public class TerrainChunk : MonoBehaviour
         mesh = new Mesh();
         GetComponent<MeshFilter>().mesh = mesh;
         edgeCollider = GetComponent<EdgeCollider2D>();
+    }
+
+    private void Update()
+    {
+        if (treeLocalPositionsPerType == null || treeMaterials == null || treeMesh == null) return;
+
+        if (treeMatricesPerType == null || treeMatricesPerType.Length != treeMaterials.Length)
+        {
+            treeMatricesPerType = new Matrix4x4[treeMaterials.Length][];
+        }
+
+        for (int typeIndex = 0; typeIndex < treeMaterials.Length; typeIndex++)
+        {
+            Vector3[] localPosArray = treeLocalPositionsPerType[typeIndex];
+
+            if (localPosArray == null || localPosArray.Length == 0) continue;
+
+            if (treeMatricesPerType[typeIndex] == null || treeMatricesPerType[typeIndex].Length != localPosArray.Length)
+            {
+                treeMatricesPerType[typeIndex] = new Matrix4x4[localPosArray.Length];
+            }
+
+            for (int i = 0; i < localPosArray.Length; i++)
+            {
+                Vector3 worldPos = transform.position + localPosArray[i];
+                treeMatricesPerType[typeIndex][i] = Matrix4x4.TRS(worldPos, Quaternion.identity, treeScalesPerType[typeIndex][i]);
+            }
+
+            Graphics.DrawMeshInstanced(treeMesh, 0, treeMaterials[typeIndex], treeMatricesPerType[typeIndex]);
+        }
     }
 
     public async void GenerateChunkAsync(float globalXOffset)
@@ -67,11 +112,10 @@ public class TerrainChunk : MonoBehaviour
         {
             grassTopRenderer.positionCount = data.colliderPoints.Length;
 
-            grassTopRenderer.sortingLayerName = "Ground"; // Той самий шар, що й у землі
-            grassTopRenderer.sortingOrder = 10;            // Але цифра 1 (поверх землі!)
+            grassTopRenderer.sortingLayerName = "Ground"; 
+            grassTopRenderer.sortingOrder = 10;           
 
-            // Змінна для висоти трави (можеш винести її вгору скрипта як public float grassOffset)
-            float grassOffset = 0.15f; // Чим більше число, тим вище над землею буде трава
+            float grassOffset = 0.15f; // the higher value the higher grass
 
             for (int i = 0; i < data.colliderPoints.Length; i++)
             {
@@ -93,6 +137,9 @@ public class TerrainChunk : MonoBehaviour
         mesh.RecalculateBounds();
 
         edgeCollider.points = data.colliderPoints;
+
+        treeLocalPositionsPerType = data.localPositionsPerType;
+        treeScalesPerType = data.scalesPerType;
 
         SpawnEnemy(data.colliderPoints);
     }
@@ -159,7 +206,73 @@ public class TerrainChunk : MonoBehaviour
             tris += 6;
         }
 
-        return new ChunkData { vertices = fullVertices, triangles = triangles, uvs = uvs, colliderPoints = colliderPoints };
+        int typesCount = treeMaterials != null ? treeMaterials.Length : 0;
+
+        List<Vector3>[] localPosLists = new List<Vector3>[typesCount];
+        List<Vector3>[] scaleLists = new List<Vector3>[typesCount];
+
+        for (int i = 0; i < typesCount; i++)
+        {
+            localPosLists[i] = new List<Vector3>();
+            scaleLists[i] = new List<Vector3>();
+        }
+
+        System.Random prng = new System.Random((int)(globalXOffset * 1000f + seed));
+        float lastTreeX = -9999f;
+
+        for (int i = 1; i < colliderPoints.Length - 1; i++)
+        {
+            Vector2 currentPoint = colliderPoints[i];
+
+            if (currentPoint.x - lastTreeX < minTreeDistance) continue;
+
+            if (typesCount > 0 && prng.NextDouble() <= treeDensity)
+            {
+                Vector2 nextPoint = colliderPoints[i + 1];
+                Vector2 prevPoint = colliderPoints[i - 1];
+                Vector2 surfaceDirection = (nextPoint - prevPoint).normalized;
+
+                float slopeAngle = Vector2.Angle(Vector2.right, surfaceDirection);
+                if (slopeAngle > 90f) slopeAngle = 180f - slopeAngle;
+
+                if (slopeAngle <= maxSlopeAngle)
+                {
+                    int randomTreeType = prng.Next(0, typesCount);
+
+                    float randomScaleY = (float)prng.NextDouble() * 0.5f + 2f;
+                    float randomScaleX = randomScaleY;
+                    Vector3 scale = new Vector3(randomScaleX, randomScaleY, 1f);
+
+                    float correctedY = currentPoint.y + (randomScaleY / 2f);
+
+                    Vector3 localPos = new Vector3(currentPoint.x, correctedY, -9f);
+
+                    localPosLists[randomTreeType].Add(localPos);
+                    scaleLists[randomTreeType].Add(scale);
+
+                    lastTreeX = currentPoint.x;
+                }
+            }
+        }
+
+        Vector3[][] finalLocalPos = new Vector3[typesCount][];
+        Vector3[][] finalScales = new Vector3[typesCount][];
+
+        for (int i = 0; i < typesCount; i++)
+        {
+            finalLocalPos[i] = localPosLists[i].ToArray();
+            finalScales[i] = scaleLists[i].ToArray();
+        }
+
+        return new ChunkData
+        {
+            vertices = fullVertices,
+            triangles = triangles,
+            uvs = uvs,
+            colliderPoints = colliderPoints,
+            localPositionsPerType = finalLocalPos, // Передаємо згруповані дані
+            scalesPerType = finalScales
+        };
     }
 
     private void SpawnEnemy(Vector2[] colliderPoints)
